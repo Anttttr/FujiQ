791d4007d8f23e7bd42f9751fc0cad358aaad4a1
Anttttr <50745904+Anttttr@users.noreply.github.com>
2022-05-03 03:38:23 +0500

    Tested methods

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-id = 0
+# -*- coding: utf8 -*-
+from sql import SQL
+db = SQL('db.db')
+id = 91153
 name = ""
 q = []
 a = '0'
-
-while a == '0':
-    id = input()
-    a = input()
-    if id not in q:
-        q.append(id)
-    else:
-        q.remove(id)
-    print(q)
+#
+# while a == '0':
+#     id = input()
+#     a = input()
+#     if id not in q:
+#         q.append(id)
+#     else:
+#         q.remove(id)
+#     print(q)
+print(db.get_fio(id))
+print(db.get_pos(id))
+print(db.get_flag(id))

be0faff7a13a692d89103f76e005cd46a4c3909c
Anttttr <50745904+Anttttr@users.noreply.github.com>
2022-05-02 15:33:16 +0500

    Added base for queue

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+id = 0
+name = ""
+q = []
+a = '0'
+
+while a == '0':
+    id = input()
+    a = input()
+    if id not in q:
+        q.append(id)
+    else:
+        q.remove(id)
+    print(q)
---
+++ sql.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+class SQL:
+
+    def __init__(self, database):
+        """Подключаемся к БД и сохраняем курсор соединения"""
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def get_flag(self, site_id):
+        """Получаем флаг в очереди"""
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM `user_info` WHERE `site_id` = ?", (site_id,)).fetchall()
+
+    def get_pos(self, site_id):
+        """Получаем место в очереди"""
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM `user_info` WHERE `site_id` = ?", (site_id,)).fetchall()
